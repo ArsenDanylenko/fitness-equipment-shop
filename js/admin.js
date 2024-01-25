5e035bdc3d446244ae0a03d895aa503f48c46aad
Arseniy Danylenko <103411610+ArsenDanylenko@users.noreply.github.com>
2024-01-25 00:05:08 -0600

    started writing functionality to add new item

--- js/admin.js
+++ js/admin.js
@@ -4,17 +4,30 @@ class NewItem {
       this.description = description;
       this.price = price;
       this.isInStock = isInStock;
+      this.addEventListener();
    }
 
-   get statusLabel() {
-      return this.isInStock ? "In stock" : "Out of stock";
+   // get statusLabel() {
+   //    return this.isInStock ? "In stock" : "Out of stock";
+   // }
+
+   addEventListener() {
+      const form = document.querySelector('#addNewProductForm');
+      form.addEventListener('submit', this.addNewProduct.bind(this));
    }
+
+   addNewProduct(event) {
+      // event.preventDefault();
+      const newTitle = document.getElementById('#newProductTitle');
+      const newDescription = document.getElementById('#newProductDescription');
+      const newPrice = document.getElementById('#newProductPrice');
+      const inStock = form.isInStock;
+
+      const newItem = new Product(newTitle.value, newDescription.value, newPrice.value, inStock.checked);
+
+      console.log(inStock);
+   }
+
+
 }
-
-form.onsubmit = (e) => {
-   e.preventDefault();
-
-   const newItem = new Product(modelIn.value, priceIn.value, statusCb.checked);
-
-   addProductToTable(newItem);
-}
+new NewItem().addEventListener();
