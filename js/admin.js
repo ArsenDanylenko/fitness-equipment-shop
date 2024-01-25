c299559d9b5b830754ac5034c624d1038b260e9b
Arseniy Danylenko <103411610+ArsenDanylenko@users.noreply.github.com>
2024-01-25 11:08:59 -0600

    started creating a function to add new products

--- js/admin.js
+++ js/admin.js
@@ -1,33 +1,33 @@
+import { ProductsService } from "./products-service.js";
+
+const form = document.querySelector('#addNewProductForm');
+
 class NewItem {
+   static count = 5;
+
    constructor(title, description, price, isInStock) {
+      this.productsService = new ProductsService();
       this.title = title;
       this.description = description;
       this.price = price;
       this.isInStock = isInStock;
-      this.addEventListener();
+      this.id = NewItem.count++
    }
-
-   // get statusLabel() {
-   //    return this.isInStock ? "In stock" : "Out of stock";
-   // }
-
-   addEventListener() {
-      const form = document.querySelector('#addNewProductForm');
-      form.addEventListener('submit', this.addNewProduct.bind(this));
-   }
-
-   addNewProduct(event) {
-      // event.preventDefault();
-      const newTitle = document.getElementById('#newProductTitle');
-      const newDescription = document.getElementById('#newProductDescription');
-      const newPrice = document.getElementById('#newProductPrice');
-      const inStock = form.isInStock;
-
-      const newItem = new Product(newTitle.value, newDescription.value, newPrice.value, inStock.checked);
-
-      console.log(inStock);
-   }
-
-
 }
-new NewItem().addEventListener();
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   const newTitle = document.getElementById('newProductTitle');
+   const newDescription = document.getElementById('newProductDescription');
+   const newPrice = document.getElementById('newProductPrice');
+   const inStock = form.querySelector('input[id="inStock"]:checked');
+
+   const newItem = new NewItem(newTitle.value, newDescription.value, newPrice.value, inStock ? true : false);
+   addNewItem(newItem);
+});
+
+async function addNewItem(newItem) {
+   const products = await newItem.productsService.getProducts();
+   products.push(newItem);
+   JSON.stringify(products);
+}
