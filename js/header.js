89e772b1b49711cfa26847ff563decca3af2d10f
Arseniy Danylenko <103411610+ArsenDanylenko@users.noreply.github.com>
2024-01-27 00:22:43 -0600

    created a function that allows searching a product via search input

--- js/header.js
+++ js/header.js
@@ -21,3 +21,26 @@ function showBurger() {
    })
 }
 showBurger();
+
+// ==== SEARCH PRODUCTS ====
+
+async function searchProducts() {
+   try {
+      const searchInput = document.querySelector('.nav-menu__search-input');
+      const productList = document.querySelector('.product-list');
+
+      searchInput.addEventListener('input', (e) => {
+         const value = e.target.value.trim().toLowerCase();
+
+         for (const productCard of productList.children) {
+            const title = productCard.querySelector('.product-title').textContent.toLowerCase();
+            const description = productCard.querySelector('.product-description').textContent.toLowerCase();
+            const isVisible = title.includes(value) || description.includes(value);
+            productCard.classList.toggle('hidden', !isVisible);
+         }
+      });
+   } catch (error) {
+      console.error(error);
+   }
+}
+searchProducts();
